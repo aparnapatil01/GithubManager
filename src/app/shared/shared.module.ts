1e04ab641c1bed2d70379b4c58936de37722b977
aparnap01 <aparna.well@gmail.com>
2019-08-02 13:31:55 +0530

    Created pagination module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { FormsModule } from '@angular/forms';
+
+import { PaginationModule } from './pagination/pagination.module';
 
 import { FilterTextboxComponent } from './filter-textbox/filter-textbox.component';
 
 @NgModule({
   imports: [
     CommonModule,
-    FormsModule
+    FormsModule,
+    PaginationModule
   ],
-  exports: [FormsModule, FilterTextboxComponent],
+  exports: [FormsModule, PaginationModule, FilterTextboxComponent],
   declarations: [FilterTextboxComponent]
 })
 export class SharedModule { }
